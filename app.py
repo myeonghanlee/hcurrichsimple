0f86673c3515694282ff6e472e8525549352dba7
myeonghanlee <myeonghan.lee1@gmail.com>
2026-08-19 08:50:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -155,17 +155,13 @@ st.title("📚 고등학교 교육과정 편성 자율 점검 시스템 (v2.9)")
 with st.container(border=True):
     st.markdown(
         """
-        <style>
-        .stMarkdown p {
-        line-height: 1.1 !important;
-        margin-bottom: 5px !important;
-        }
-        </style>
+        <div style="line-height: 1.0; margin-top: 10px;">
+        2022 개정 교육과정 기반으로 제작(2026.7.19., by lmh)되었으며, 2027학년도 교육과정 편제 검토용입니다.<br>
+        업데이트는 없을 예정이며, 어디까지나 임시 체크용으로만 사용하시기 바랍니다.
+        </div>
         """,
         unsafe_allow_html=True
     )
-    st.markdwon("2022 개정 교육과정 기반으로 제작(2026.7.19., by lmh)되었으며, 2027학년도 교육과정 편제 검토용입니다.")
-    st.write("업데이트는 없을 예정이며, 어디까지나 임시 체크용으로만 사용하시기 바랍니다.")
 
 if 'uploader_key' not in st.session_state:
     st.session_state.uploader_key = 0
